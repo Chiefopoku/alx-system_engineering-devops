1048019f0adead16b26f4b42b9f8d6d6bbb688bb
root <kwabenaopokujnr@gmail.com>
2024-05-07 14:31:41 -0700

    add

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,24 +2,16 @@
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
-
 import requests
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit.
-
-    If the subreddit is invalid and leads to a redirect (commonly to a search results page), return 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyRedditClient/1.0"}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            # This includes any response code that isn't 200, e.g., 301, 302, 404, etc.
-            return 0
-    except requests.RequestException as e:
-        print(f"An error occurred: {e}")
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
